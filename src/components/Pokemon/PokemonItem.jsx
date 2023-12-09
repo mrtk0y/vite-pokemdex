75082c022e6161cba4ef24989fcdf10a65e32b7b
manhcuongdtbk <manhcuongdtbk@gmail.com>
2023-12-09 11:26:40 +0700

    move PokemonItem component to Pokemon related component folder

--- src/components/Pokemon/PokemonItem.jsx
+++ src/components/Pokemon/PokemonItem.jsx
@@ -1,19 +1,17 @@
-// TODO: how to use this component to display each Pokemon?
-
-const Pokemon = ({ imageUrl, id, name, typeNames, isShow }) => {
+const PokemonItem = ({ imageUrl, id, name, typeNames, isShow }) => {
   return (
     // TODO: convert css for `isHidden` class
     <div className={`${isShow ? "" :"isHidden"} m-2 h-4/5 w-36 rounded-lg bg-white capitalize flex flex-col justify-center items-center`}>
-      <img src={imageUrl} />
+      <img src={imageUrl} alt='pokemon image' />
       <p>N° {id}</p>
       <h3><strong>{name}</strong></h3>
       <div className="flex justify-around items-center capitalize mb-3.5 gap-2">
         {typeNames.map((typeName) => (
-          <p key={typeName} className="rounded bg-green-300 px-3">{typeName}</p>
+          <p key={typeName} className="rounded px-3 bg-green-300 ">{typeName}</p>
         ))}
       </div>
     </div>
   )
 }
 
-export default Pokemon
+export default PokemonItem
